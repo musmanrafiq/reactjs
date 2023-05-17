1fe5abe62f11452571e82607664f0a6d97a13f57
Usman <usman.solemn@gmail.com>
2023-05-17 18:56:50 +0500

    Add button click

--- react18_exercises/src/App.js
+++ react18_exercises/src/App.js
@@ -2,22 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const onButtonClick = () => {
+    alert('Hi')
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <button onClick={onButtonClick}>Click me</button>
     </div>
   );
 }
